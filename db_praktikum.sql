bead4f5312b45ab766d172ce284aaa26023aaeea
Salsabilla agustin <salsabillaagustin3@gmail.com>
2026-06-23 22:33:35 +0700

    sql

---
+++ db_praktikum.sql
@@ -0,0 +1,270 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jun 23, 2026 at 03:32 PM
+-- Server version: 8.0.30
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_praktikum`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `asisten_lab`
+--
+
+CREATE TABLE `asisten_lab` (
+  `id_asisten` int NOT NULL,
+  `nama_asisten` varchar(100) DEFAULT NULL,
+  `no_hp` varchar(13) DEFAULT NULL,
+  `id_user` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detail_jadwal`
+--
+
+CREATE TABLE `detail_jadwal` (
+  `id_detail` int NOT NULL,
+  `id_jadwal` int DEFAULT NULL,
+  `id_praktikum` int DEFAULT NULL,
+  `id_asisten` int DEFAULT NULL,
+  `id_kelas` int DEFAULT NULL,
+  `hari` varchar(20) DEFAULT NULL,
+  `jam_mulai` time DEFAULT NULL,
+  `jam_selesai` time DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jadwal`
+--
+
+CREATE TABLE `jadwal` (
+  `id_jadwal` int NOT NULL,
+  `tanggal` date DEFAULT NULL,
+  `id_lab` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kelas`
+--
+
+CREATE TABLE `kelas` (
+  `id_kelas` int NOT NULL,
+  `nama_kelas` varchar(50) DEFAULT NULL,
+  `program_studi` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `laboratorium`
+--
+
+CREATE TABLE `laboratorium` (
+  `id_lab` int NOT NULL,
+  `nama_lab` varchar(5) DEFAULT NULL,
+  `kapasitas` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `nim` varchar(10) NOT NULL,
+  `nama_mahasiswa` varchar(100) DEFAULT NULL,
+  `jurusan` varchar(50) DEFAULT NULL,
+  `id_kelas` int DEFAULT NULL,
+  `id_user` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `praktikum`
+--
+
+CREATE TABLE `praktikum` (
+  `id_praktikum` int NOT NULL,
+  `nama_praktikum` varchar(50) DEFAULT NULL,
+  `semester` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id_user` int NOT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(10) DEFAULT NULL,
+  `role` enum('admin','asisten','mahasiswa') DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `asisten_lab`
+--
+ALTER TABLE `asisten_lab`
+  ADD PRIMARY KEY (`id_asisten`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Indexes for table `detail_jadwal`
+--
+ALTER TABLE `detail_jadwal`
+  ADD PRIMARY KEY (`id_detail`),
+  ADD KEY `id_jadwal` (`id_jadwal`),
+  ADD KEY `id_praktikum` (`id_praktikum`),
+  ADD KEY `id_asisten` (`id_asisten`),
+  ADD KEY `id_kelas` (`id_kelas`);
+
+--
+-- Indexes for table `jadwal`
+--
+ALTER TABLE `jadwal`
+  ADD PRIMARY KEY (`id_jadwal`),
+  ADD KEY `id_lab` (`id_lab`);
+
+--
+-- Indexes for table `kelas`
+--
+ALTER TABLE `kelas`
+  ADD PRIMARY KEY (`id_kelas`);
+
+--
+-- Indexes for table `laboratorium`
+--
+ALTER TABLE `laboratorium`
+  ADD PRIMARY KEY (`id_lab`);
+
+--
+-- Indexes for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`nim`),
+  ADD KEY `id_kelas` (`id_kelas`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Indexes for table `praktikum`
+--
+ALTER TABLE `praktikum`
+  ADD PRIMARY KEY (`id_praktikum`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `asisten_lab`
+--
+ALTER TABLE `asisten_lab`
+  MODIFY `id_asisten` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `detail_jadwal`
+--
+ALTER TABLE `detail_jadwal`
+  MODIFY `id_detail` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `jadwal`
+--
+ALTER TABLE `jadwal`
+  MODIFY `id_jadwal` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `kelas`
+--
+ALTER TABLE `kelas`
+  MODIFY `id_kelas` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `laboratorium`
+--
+ALTER TABLE `laboratorium`
+  MODIFY `id_lab` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `praktikum`
+--
+ALTER TABLE `praktikum`
+  MODIFY `id_praktikum` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id_user` int NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `asisten_lab`
+--
+ALTER TABLE `asisten_lab`
+  ADD CONSTRAINT `asisten_lab_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`);
+
+--
+-- Constraints for table `detail_jadwal`
+--
+ALTER TABLE `detail_jadwal`
+  ADD CONSTRAINT `detail_jadwal_ibfk_1` FOREIGN KEY (`id_jadwal`) REFERENCES `jadwal` (`id_jadwal`),
+  ADD CONSTRAINT `detail_jadwal_ibfk_2` FOREIGN KEY (`id_praktikum`) REFERENCES `praktikum` (`id_praktikum`),
+  ADD CONSTRAINT `detail_jadwal_ibfk_3` FOREIGN KEY (`id_asisten`) REFERENCES `asisten_lab` (`id_asisten`),
+  ADD CONSTRAINT `detail_jadwal_ibfk_4` FOREIGN KEY (`id_kelas`) REFERENCES `kelas` (`id_kelas`);
+
+--
+-- Constraints for table `jadwal`
+--
+ALTER TABLE `jadwal`
+  ADD CONSTRAINT `jadwal_ibfk_1` FOREIGN KEY (`id_lab`) REFERENCES `laboratorium` (`id_lab`);
+
+--
+-- Constraints for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD CONSTRAINT `mahasiswa_ibfk_1` FOREIGN KEY (`id_kelas`) REFERENCES `kelas` (`id_kelas`),
+  ADD CONSTRAINT `mahasiswa_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
