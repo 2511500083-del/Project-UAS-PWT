2a1aec6f63ecc3db5e6fd566ed14688bf3ea2fac
irsya eva safitri <2511500085@mahasiswa.atmaluhur.ac.id>
2026-06-23 23:03:32 +0700

    simpan semua kode

--- auth/proses_login.php
+++ auth/proses_login.php
@@ -1,38 +1,55 @@
 <?php
+
 session_start();
+
 include '../config/koneksi.php';
 
 $username = $_POST['username'];
 $password = $_POST['password'];
 
 $query = mysqli_query($conn,
-"SELECT * FROM users WHERE username='$username'");
+"SELECT * FROM users
+WHERE username='$username'
+AND password='$password'
+");
 
 $data = mysqli_fetch_assoc($query);
 
 if($data){
 
-    if(password_verify($password,$data['password'])){
+    $_SESSION['id_user'] = $data['id_user'];
 
-        $_SESSION['id_user'] = $data['id_user'];
-        $_SESSION['role'] = $data['role'];
+    $_SESSION['role'] = $data['role'];
 
-        if($data['role']=="admin"){
-            header("Location: ../admin/dashboard.php");
-        }
+    if($data['role']=='admin'){
 
-        if($data['role']=="asisten"){
-            header("Location: ../asisten/dashboard.php");
-        }
+        header("Location: ../admin/dashboard.php");
 
-        if($data['role']=="mahasiswa"){
-            header("Location: ../mahasiswa/dashboard.php");
-        }
+    }
 
-    }else{
-        echo "Password salah";
+    elseif($data['role']=='asisten'){
+
+        header("Location: ../asisten/dashboard.php");
+
+    }
+
+    elseif($data['role']=='mahasiswa'){
+
+        header("Location: ../mahasiswa/dashboard.php");
+
     }
 
 }else{
-    echo "User tidak ditemukan";
+
+    echo "
+    <script>
+
+    alert('Username atau Password Salah');
+
+    window.location='login.php';
+
+    </script>
+    ";
+
 }
+?>
